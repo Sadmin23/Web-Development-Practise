c73211b4f79de14520e65a06f9d88a47f0f0a995
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-02-06 21:01:46 +0600

    error fixed

--- Fruits/app.js
+++ Fruits/app.js
@@ -1,39 +1,23 @@
 const { MongoClient } = require("mongodb");
-const assert = require("assert")
 
 const url =
   "mongodb://0.0.0.0:27017";
 
 const dbName = "fruitsDB";
 
-const client = new MongoClient(url);
+MongoClient.connect(url).then((client) => {
+  
+  const connect = client.db(dbName);
 
-async function run() {
-  try {
-    const database = client.db(dbName);
-    const movies = database.collection("fruits");
+  // Connect to collection
+  const collection = connect
+      .collection("fruits");
 
-    // query for movies that have a runtime less than 15 minutes
-//    const query = { runtime: { $lt: 15 } };
+  collection.find({}).toArray().then((ans) => {
+      console.log(ans);
+  });
+}).catch((err) => {
 
-    const options = {
-      // sort returned documents in ascending order by title (A->Z)
-  //    sort: { title: 1 },
-      // Include only the `title` and `imdb` fields in each returned document
-      projection: { _id: 0, name: 1, score: 1, review: 1},
-    };
-
-    const cursor = movies.find(options);
-
-    // print a message if no documents were found
-    if ((await cursor.count()) === 0) {
-      console.log("No documents found!");
-    }
-
-    // replace console.dir with your callback to access individual elements
-    await cursor.forEach(console.dir);
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+  // Printing the error message
+  console.log(err.Message);
+})

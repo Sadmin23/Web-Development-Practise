09aacc2451def9f9cb09bd4bf2bb3e54c89e30c1
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-02-08 18:02:45 +0600

    data added

--- Fruits/app.js
+++ Fruits/app.js
@@ -2,6 +2,7 @@
 
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {useNewUrlParser: true});
 
 const fruitSchema = new mongoose.Schema ({
@@ -17,3 +18,41 @@ const fruit = new Fruit ({
   rating: 7,
   review: "Pretty solid as a fruit." 
 });
+
+const personSchema = new mongoose.Schema ({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model("Person", fruitSchema);
+
+const person = new Person ({
+  name: "John",
+  rating: 37 
+});
+
+const kiwi = new Fruit ({
+  name: "Kiwi",
+  rating: 10,
+  review: "The best fruit!" 
+});
+
+const orange = new Fruit ({
+  name: "Orange",
+  rating: 4,
+  review: "Too sour for me" 
+});
+
+const banana = new Fruit ({
+  name: "Banana",
+  rating: 3,
+  review: "Weird texture" 
+});
+
+Fruit.insertMany([fruit, kiwi, orange, banana], function(err){
+  if (err){
+    console.log(err);
+  } else {
+    console.log("Successfully added data");
+  }
+})
